8b02964591ec6ae1f1d0cf08e4ea10b24b0795a5
rubenwardy <rubenwardy@gmail.com>
2016-03-19 22:41:25 +0000

    Allow '-' in modnames

--- app/src/main/java/com/rubenwardy/minetestmodmanager/manager/ModInstallService.java
+++ app/src/main/java/com/rubenwardy/minetestmodmanager/manager/ModInstallService.java
@@ -63,7 +63,7 @@ public class ModInstallService extends IntentService {
     private boolean isValidModname(String str) {
         for (int i = 0; i < str.length(); ++i) {
             char c = str.charAt(i);
-            if (!Character.isDigit(c) && !Character.isLetter(c) && c != '_') {
+            if (!Character.isDigit(c) && !Character.isLetter(c) && c != '_' && c != '-') {
                 return false;
             }
         }
